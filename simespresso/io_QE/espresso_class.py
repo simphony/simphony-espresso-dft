5432f95bfcd93376c2a56a748079c4c4b248ca1c
jr <jeremy@trendiguru.com>
2015-12-05 15:10:20 +0200

    more pep8 ftw

--- simespresso/io_QE/espresso_class.py
+++ simespresso/io_QE/espresso_class.py
@@ -70,7 +70,7 @@ class qe_functions(object):
                 n_lattice_points = ints[0:3]
                 n_atoms = ints[6]
                 logging.debug('n_points:' + str(n_lattice_points)
-                      + ' n_atoms:' + str(n_atoms))
+                    + ' n_atoms:' + str(n_atoms))
 
                 # 3rd line : bravais lattice, celldm[0],[1],[2]
                 line = file_iter.next()
@@ -85,7 +85,8 @@ class qe_functions(object):
                 bravais = int(floats[0])
                 celldm = floats[1:4]
 #                SP[CUBA.ORIGINAL_POSITION] = [celldm[0],celldm[1],celldm[2]]
-                logging.debug('bravais:' + str(bravais) + ' celldm:' + str(celldm))
+                logging.debug('bravais:' + str(bravais) +
+                    ' celldm:' + str(celldm))
                 # see http://www.quantum-espresso.org/
                 # wp-content/uploads/Doc/INPUT_PW.html#idp82064
                 if bravais == 0:
