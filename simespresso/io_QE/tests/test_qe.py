803126861ff82cd1bc99227390bb9e78001a0a9e
jeremy <jeremy@trendiguru.com>
2016-06-28 14:48:35 +0100

    xyz read and display (matplotlib) - pep complaints by travis...

--- simespresso/io_QE/tests/test_qe.py
+++ simespresso/io_QE/tests/test_qe.py
@@ -132,42 +132,43 @@ class OutcomesTest(unittest.TestCase):
         qe_wrapper = qe_file_io.qe_functions()
         qe_wrapper.QeFileIoDataManager._read_espresso_input_file(file_name)
 
+
     def test_running_index_to_node_inredex(self):
         print('testing espresso_data_file_read')
         n_latticepoints = [10, 7, 6]
         index = 5
-        espresso_functions = qe_file_io.qe_functions
-        indices = espresso_functions.running_index_to_node_index(index,
+        qeio = qe_wrapper.QeFileIoDataManager
+        indices = qeio._running_index_to_node_index(index,
                                                                n_latticepoints)
         print('index '+str(index)+' indices:'+str(indices))
         self.assertTrue(indices == [5, 0, 0])
 
         index = 15
-        indices = espresso_wrapper.running_index_to_node_index(index,
-                                                               n_latticepoints)
+        qew = qe_wrapper.QeFileIoDataManager
+        indices = qew._running_index_to_node_index(index, n_latticepoints)
         print('index '+str(index)+' indices:'+str(indices))
         self.assertTrue(indices == [5, 1, 0])
 
         index = 23
-        indices = espresso_wrapper.running_index_to_node_index(index,
+        indices = qe_wrapper.running_index_to_node_index(index,
                                                                n_latticepoints)
         print('index '+str(index)+' indices:'+str(indices))
         self.assertTrue(indices == [3, 2, 0])
 
         index = 70
-        indices = espresso_wrapper.running_index_to_node_index(index,
+        indices = qe_wrapper.running_index_to_node_index(index,
                                                                n_latticepoints)
         print('index '+str(index)+' indices:'+str(indices))
         self.assertTrue(indices == [0, 0, 1])
 
         index = 75
-        indices = espresso_wrapper.running_index_to_node_index(index,
+        indices = qe_wrapper.running_index_to_node_index(index,
                                                                n_latticepoints)
         print('index '+str(index)+' indices:'+str(indices))
         self.assertTrue(indices == [5, 0, 1])
 
         index = 150
-        indices = espresso_wrapper.running_index_to_node_index(index,
+        indices = qe_wrapper.running_index_to_node_index(index,
                                                                n_latticepoints)
         print('index '+str(index)+' indices:'+str(indices))
         self.assertTrue(indices == [0, 1, 2])
